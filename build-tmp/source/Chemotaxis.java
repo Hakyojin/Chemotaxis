43692fc4efd86e9aedc0c1540c13f9214ac6ddf7
Aidan F <aidanferrer@earthlink.net>
2015-09-23 08:18:00 -0700

    Oh man, lines and shiz

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Chemotaxis extends PApplet {
+
 public static final int bacNum = 15;
 
 Bacteria[] coleIsGod = new Bacteria[bacNum];
- void setup()   
+ public void setup()   
  {  
  	for(int i=0;i<bacNum;i++)
  	{
@@ -9,7 +25,7 @@ Bacteria[] coleIsGod = new Bacteria[bacNum];
  	}   
  	size(800,800);
  } 
- void draw()
+ public void draw()
  {
  	background(255);
  	for(int i=0;i<bacNum;i++)
@@ -36,14 +52,23 @@ Bacteria[] coleIsGod = new Bacteria[bacNum];
  		bacteriaY = 400+(int)(Math.random()*10);
  		pallet = color((int)(Math.random()*255),(int)(Math.random()*255),(int)(Math.random()*255));
  	}
- 	void walk()
+ 	public void walk()
  	{
  		bacteriaX +=(int)(Math.random()*7)-3;
 		bacteriaY +=(int)(Math.random()*7)-3;
  	}
- 	void show()
+ 	public void show()
  	{
  		fill(pallet);
  		ellipse(bacteriaX,bacteriaY,15,15);
  	}
  }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Chemotaxis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
